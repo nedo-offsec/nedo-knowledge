84fa893bf9bb4c643345a7fd630ad70316e26d42
disaster-offsec <platon.nickitin@yandex.ru>
2026-08-21 14:29:10 +0300

    Исправлены ошибки

--- programming/golang/test.go
+++ programming/golang/test.go
@@ -2,18 +2,27 @@ package main
 
 import (
     "fmt"
+		"sync"
 )
 
 func main() {
-	s := "При\xffвет" // невалидный байт \xff
-	for i, r := range s {
-		fmt.Printf("%d: %U\n", i, r)
-	}
-	// 0: U+041F
-	// 2: U+0440
-	// 4: U+0438
-	// 6: U+FFFD (ошибка)
-	// 7: U+0432
-	// 9: U+0435
-	// 11: U+0442
+    var once sync.Once
+    
+    defer func() {
+        if r := recover(); r != nil {
+            fmt.Println("Поймали панику:", r)
+        }
+    }()
+    
+    // 1
+    once.Do(func() {
+        fmt.Println("Выполняем...")
+        panic("ошибка!")
+    })
+    
+    // Этот не вызов выполнится!
+    once.Do(func() {
+        fmt.Println("Повторная попытка")
+    })
 }
+
